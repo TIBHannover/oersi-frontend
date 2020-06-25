35acd952063158a9356b472abd893baa1bc51c52
Edmond Kacaj <programim95@gmail.com>
2020-06-25 15:40:36 +0200

    Make footer customizable through html Ref https://gitlab.com/oersi/oersi-frontend/-/issues/9

--- src/__tests__/src/footerComponent/FooterComponent.test.js
+++ src/__tests__/src/footerComponent/FooterComponent.test.js
@@ -2,50 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 import {act} from "react-dom/test-utils"
 import FooterComponent from "../../../components/footerComponent/FooterComponent"
-const footerFakeData = {
-  footerLinks: [
-    {
-      title: "About",
-      links: [
-        {
-          text: "Impressum",
-          link: "https://www.tib.eu/de/service/impressum",
-        },
-        {
-          text: "Datenschutz",
-          link: "https://www.tib.eu/de/service/datenschutz",
-        },
-      ],
-    },
-    {
-      title: "Technisch",
-      links: [
-        {
-          text: "GitLab",
-          link: "https://gitlab.com/oersi",
-        },
-        {
-          text: "Issues",
-          link: "https://gitlab.com/groups/oersi/-/issues",
-        },
-      ],
-    },
-  ],
-  footerImage: [
-    {
-      image: "TIB_Logo_en.png",
-      link: "https://www.tib.eu/de/",
-    },
-    {
-      image: "Hbz-Logo.svg",
-      link: "https://www.hbz-nrw.de/",
-    },
-  ],
-}
+const footerFakehtml = "<p>this is a test</p>"
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
-    json: () => Promise.resolve(footerFakeData),
+    text: () => Promise.resolve(footerFakehtml),
   })
 )
 // jest.setTimeout(3000);
--- src/components/footerComponent/FooterComponent.js
+++ src/components/footerComponent/FooterComponent.js
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from "react"
 import "./FooterComponent.css"
 import {withTranslation} from "react-i18next"
 import {getConfiguration} from "../../service/configuration/configurationService"
-import LinkComponent from "../linkComponent/LinkComponent"
 import PropTypes from "prop-types"
+import parse from "html-react-parser"
 
 /**
  * This is the Footer component, You can use different url and image after Build
@@ -13,86 +13,28 @@ import PropTypes from "prop-types"
  */
 
 const FooterComponent = (props) => {
-  const [data, setdata] = useState({})
+  const [data, setdata] = useState("")
   const [isLoaded, setisLoaded] = useState(false)
 
   useEffect(() => {
     async function fetchData() {
-      const res = await getConfiguration("/footer/config.json")
-      const json = await res.json()
-      if (json != null) {
-        setdata(json)
+      const res = await getConfiguration("/footer/footer.html")
+      const htmlResponse = await res.text()
+      if (
+        !htmlResponse.includes("html") ||
+        !htmlResponse.includes("head") ||
+        !htmlResponse.includes("body")
+      ) {
+        setdata(htmlResponse)
         setisLoaded(true)
+      } else {
+        setisLoaded(false)
       }
     }
     fetchData()
   }, [])
 
-  return (
-    <footer className="footer">
-      {isLoaded ? (
-        <div className="container-fluid">
-          <br />
-          <div className="row">
-            <div className="col-sm-2"></div>
-            <div className="col-sm-4 footer-block">
-              <h5>{props.t("FOOTER.TITLE")}</h5>
-              <hr />
-              <div className="col-sm-12">
-                <p className="fotter-p">{props.t("FOOTER.TEXT")}</p>
-              </div>
-            </div>
-            {data.footerLinks !== "undefined" &&
-              data.footerLinks.slice(0, 2).map((dataFooter, indexFooter) => (
-                <div className="col-sm-3 footer-block" key={indexFooter}>
-                  <h5>{dataFooter.title}</h5>
-                  <hr />
-                  <ul>
-                    {dataFooter.links.map((dt, index) => (
-                      <li key={index}>
-                        <LinkComponent link={dt.link}>
-                          <span>{dt.text}</span>
-                        </LinkComponent>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-            <div className="col-sm-2"></div>
-            <br />
-            <div className="col-sm-8">
-              <hr />
-            </div>
-            <div className="col-sm-12">
-              <div className="row">
-                <div className="col-sm-2"></div>
-                {data.footerImage !== "undefined" &&
-                  data.footerImage.slice(0, 3).map((dt, index) => (
-                    <div className="col-sm-3" key={index}>
-                      <LinkComponent link={dt.link}>
-                        <img
-                          className="img-block"
-                          src={process.env.PUBLIC_URL + "/" + dt.image}
-                          alt="LOGO"
-                        />
-                      </LinkComponent>
-                    </div>
-                  ))}
-                <div className="col-sm-1"></div>
-              </div>
-            </div>
-          </div>
-          <p className="copyright pull-right">
-            <b>© {new Date().getFullYear()}</b>
-            {"  "}
-            {props.t("FOOTER.COPY_RIGHT")}
-          </p>
-        </div>
-      ) : (
-        ""
-      )}
-    </footer>
-  )
+  return <div data-insert-template-id="footer-id">{isLoaded && parse(data)}</div>
 }
 
 FooterComponent.propTypes = {
