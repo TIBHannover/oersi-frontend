19dc3c5415bfec5993dbcd2905b2b4a6b81e2671
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2020-09-02 09:24:41 +0200

    Set German translation as default
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/25

--- public/config/config.js
+++ public/config/config.js
@@ -4,7 +4,7 @@ window['runTimeConfig'] = {
     CREDENCIAL: "",
     APP_NAME: "oer_data"
   },
-  LANGUAGE: "en",
+  LANGUAGE: "de",
   GENERAL_CONFIGURATION:{
     RESULT_PAGE_SIZE_OPTIONS:["5", "10", "15", "20", "50", "100"],  // page size options configuration    
     NR_OF_RESULT_PER_PAGE:10,  //  number of results to show per view. Defaults to 10.   
