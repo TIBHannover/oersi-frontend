ade855511c73edcacda68a86f40f1b4455f8cd5d
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2020-09-22 10:42:51 +0200

    Add German translation for pagination componenet of antd
    Ref https://gitlab.com/oersi/oersi-frontend/-/merge_requests/35

--- src/components/configuration/Configuration.js
+++ src/components/configuration/Configuration.js
@@ -5,6 +5,9 @@ import config from "react-global-configuration"
 import ErrorComponent from "../errorComponent/ErrorComponent"
 import i18next from "i18next"
 import {ConfigurationRunTime} from "../../helpers/use-context"
+import {ConfigProvider} from "antd"
+import deDE from "antd/es/locale/de_DE"
+import enUS from "antd/es/locale/en_US"
 
 /**
  * Configuration
@@ -22,7 +25,9 @@ const Configuration = () => {
     if (ELASTIC_SEARCH !== null && ELASTIC_SEARCH.URL && ELASTIC_SEARCH.APP_NAME) {
       return (
         <ConfigurationRunTime.Provider value={GENERAL_CONFIGURATION}>
-          <App config={config} elasticSearch={ELASTIC_SEARCH} />
+          <ConfigProvider locale={i18next.language === "de" ? deDE : enUS}>
+            <App config={config} elasticSearch={ELASTIC_SEARCH} />
+          </ConfigProvider>
         </ConfigurationRunTime.Provider>
       )
     } else {
