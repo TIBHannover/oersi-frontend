53e3d71508729cf75bce748d8ff761de0a5b5b27
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2021-01-06 06:39:16 +0100

    Cache i18n translations
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/36

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,9 @@
 import i18n from "i18next"
 import {initReactI18next} from "react-i18next"
-import Backend from "i18next-http-backend"
+import Backend from "i18next-chained-backend"
+import HttpApi from "i18next-http-backend"
 import LanguageDetector from "i18next-browser-languagedetector"
+import LocalStorageBackend from "i18next-localstorage-backend"
 
 const fallbackLng = ["en", "de"]
 
@@ -13,16 +15,24 @@ i18n
     fallbackLng, // if user computer language is not on the list of available languages, than we will be using the fallback language specified earlier
     debug: true,
     backend: {
-      // for all available options read the backend's repository readme file
-      loadPath: (lng, namespaces) => {
-        switch (namespaces[0]) {
-          case 'lrt':
-          case 'subject':
-            return `${window.location.pathname}api-internal/label/{{lng}}`
-          default:
-            return `${window.location.pathname}locales/{{lng}}/{{ns}}.json`
-        }
-      },
+      backends: [LocalStorageBackend, HttpApi],
+      backendOptions: [
+        {
+          expirationTime: 10 * 60 * 1000,
+        },
+        {
+          // for all available options read the backend's repository readme file
+          loadPath: (lng, namespaces) => {
+            switch (namespaces[0]) {
+              case "lrt":
+              case "subject":
+                return `${window.location.pathname}api-internal/label/{{lng}}`
+              default:
+                return `${window.location.pathname}locales/{{lng}}/{{ns}}.json`
+            }
+          },
+        },
+      ],
     },
     interpolation: {
       escapeValue: false,
