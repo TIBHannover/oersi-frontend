b544ffbdc093b69b9951b4764f92896579cbd1be
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2021-01-07 08:59:04 +0100

    Custom rendering of selected filters including translation of values
    Uses material design
    
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/36

--- src/components/filterComponent/FilterComponent.js
+++ src/components/filterComponent/FilterComponent.js
@@ -6,6 +6,9 @@ import HeaderComponent from "../headerComponent/HeaderComponent"
 import MultiDropDownComponent from "../multiDropDownComponent/MultiDropDownComponent"
 import MultiListComponent from "../multiListComponent/MultiListComponent"
 import {withTranslation} from "react-i18next"
+import Button from "@material-ui/core/Button"
+import CloseIcon from "@material-ui/icons/Close"
+import {getLabelForStandardComponent} from "../../helpers/helpers"
 
 /**
  * This is the Filter component,this component is to show the filters,for diferrent view
@@ -33,6 +36,7 @@ const FilterComponent = (props) => {
           <SelectedFilters
             showClearAll={true}
             clearAllLabel={props.t("FILTER.CLEAR_ALL")}
+            render={renderSelectedFilters}
           />
           <ResultComponent />
         </div>
@@ -54,6 +58,61 @@ const FilterComponent = (props) => {
       </div>
     </>
   )
+
+  function renderValue(component, value, isArray) {
+    if (isArray && value.length) {
+      const arrayToRender = value.map((item) =>
+        renderValue(component, item, Array.isArray(item))
+      )
+      return arrayToRender.join(", ")
+    }
+    return getLabelForStandardComponent(value, component, props.t)
+  }
+
+  function renderSelectedFilters(data) {
+    const selectedValues = data.selectedValues
+    const appliedFilters = Object.keys(data.selectedValues)
+    let hasValues = false
+    return (
+      <div className="selectedFilters">
+        {appliedFilters
+          .filter(
+            (id) => data.components.includes(id) && selectedValues[id].showFilter
+          )
+          .map((component) => {
+            const {label, value} = selectedValues[component]
+            const isArray = Array.isArray(value)
+            if (label && ((isArray && value.length) || (!isArray && value))) {
+              hasValues = true
+              return (
+                <Button
+                  variant="contained"
+                  disableElevation
+                  className="m-1"
+                  key={component}
+                  onClick={() => data.setValue(component, null)}
+                  endIcon={<CloseIcon />}
+                >
+                  {selectedValues[component].label}:{" "}
+                  {renderValue(component, value, isArray)}
+                </Button>
+              )
+            }
+            return null
+          })}
+        {hasValues ? (
+          <Button
+            variant="contained"
+            disableElevation
+            className="m-1"
+            onClick={data.clearValues}
+          >
+            {data.clearAllLabel}
+          </Button>
+        ) : null}
+      </div>
+    )
+  }
 }
 
-export default withTranslation()(FilterComponent)
+export default withTranslation(["translation", "lrt", "subject"])(FilterComponent)
