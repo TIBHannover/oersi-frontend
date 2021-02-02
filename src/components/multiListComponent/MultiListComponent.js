132f4c24878ece960eb9f6d125c99dbf724af79a
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2021-02-02 07:01:16 +0100

    Adjusted style of filters, made them expandable
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/46

--- src/components/multiListComponent/MultiListComponent.js
+++ src/components/multiListComponent/MultiListComponent.js
@@ -3,43 +3,63 @@ import {MultiList} from "@appbaseio/reactivesearch"
 import "./MultiListComponent.css"
 import {getLabelForStandardComponent} from "../../helpers/helpers"
 import {withTranslation} from "react-i18next"
+import Button from "@material-ui/core/Button"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import ExpandLessIcon from "@material-ui/icons/ExpandLess"
+import Collapse from "@material-ui/core/Collapse"
 
 const MultiListComponent = (props) => {
+  const [expanded, setExpanded] = React.useState(false)
+  const handleExpandClick = () => {
+    setExpanded(!expanded)
+  }
   return (
     <div className="multilist card">
-      <div className="multilist content">
-        <div className="filter-heading center">
-          <b>
-            {" "}
-            <i className={props.fontAwesome} />{" "}
+      <div className="multilist content pl-3 pr-3">
+        <Button
+          fullWidth={true}
+          style={{justifyContent: "flex-start"}}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show filter values"
+        >
+          <div className="filter-heading">
             {props.t("CARD." + props.title.toUpperCase())}
-          </b>
-        </div>
-        {!props.showSearch && <hr className="blue" />}
-        <MultiList
-          className={props.className}
-          dataField={props.dataField}
-          // title={props.title}
-          componentId={props.component}
-          // queryFormat="or"
-          showMissing={props.showMissing}
-          missingLabel={props.t("FILTER." + props.missingLabel)}
-          placeholder={props.t("CARD." + props.placeholder.toUpperCase())}
-          showFilter={props.showFilter}
-          showSearch={props.showSearch}
-          size={props.size}
-          filterLabel={props.t("CARD." + props.filterLabel.toUpperCase())}
-          URLParams={props.URLParams}
-          react={{and: props.and}}
-          renderItem={(label, count) => onItemRender(label, count, props.component)}
-          innerClass={{
-            label: "multilist-label",
-            input: "search-input",
-            checkbox: "multilist-checkbox",
-          }}
-          customQuery={props.customQuery}
-          defaultQuery={props.defaultQuery}
-        />
+          </div>
+          {expanded ? (
+            <ExpandLessIcon className="filter-heading-icon" />
+          ) : (
+            <ExpandMoreIcon className="filter-heading-icon" />
+          )}
+        </Button>
+        <Collapse in={expanded} timeout="auto">
+          <MultiList
+            className={props.className}
+            dataField={props.dataField}
+            // title={props.title}
+            componentId={props.component}
+            // queryFormat="or"
+            showMissing={props.showMissing}
+            missingLabel={props.t("FILTER." + props.missingLabel)}
+            placeholder={props.t("CARD." + props.placeholder.toUpperCase())}
+            showFilter={props.showFilter}
+            showSearch={props.showSearch}
+            size={props.size}
+            filterLabel={props.t("CARD." + props.filterLabel.toUpperCase())}
+            URLParams={props.URLParams}
+            react={{and: props.and}}
+            renderItem={(label, count) =>
+              onItemRender(label, count, props.component)
+            }
+            innerClass={{
+              label: "multilist-label",
+              input: "search-input",
+              checkbox: "multilist-checkbox",
+            }}
+            customQuery={props.customQuery}
+            defaultQuery={props.defaultQuery}
+          />
+        </Collapse>
       </div>
     </div>
   )
