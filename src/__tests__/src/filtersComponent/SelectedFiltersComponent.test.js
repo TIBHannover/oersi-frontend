736937cd4078c67ebb158ca77fd4c234af4b37c8
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2021-02-10 14:34:03 +0100

    Refactoring: use filter-list as own component
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/46

--- src/__tests__/src/filtersComponent/SelectedFiltersComponent.test.js
+++ src/__tests__/src/filtersComponent/SelectedFiltersComponent.test.js
@@ -1,38 +1,12 @@
-import React, {Suspense} from "react"
+import React from "react"
 import ReactDOM from "react-dom"
-import {act} from "react-dom/test-utils"
-import config from "react-global-configuration"
-import prod from "../../../config/prod"
-import FilterComponent from "../../../components/filterComponent/FilterComponent"
-import {renderSelectedFilters} from "../../../components/filterComponent/FilterComponent"
-import {I18nextProvider} from "react-i18next"
-import i18n from "i18next"
-
-beforeEach(() => {
-  // setup a config file
-  config.set(prod, {freeze: false})
-})
+import {renderSelectedFilters} from "../../../components/filtersComponent/SelectedFiltersComponent"
 
 function translateDummy(key, options) {
   return key + "_translated"
 }
 
-describe("FilterComponent ==> Test UI  ", () => {
-  it("FilterComponent : should render without crashing", async () => {
-    const div = document.createElement("div")
-    await act(async () => {
-      ReactDOM.render(
-        <I18nextProvider i18n={i18n}>
-          <Suspense fallback={<div>Loading translations...</div>}>
-            <FilterComponent multilist={config.get("multiList")} />
-          </Suspense>
-        </I18nextProvider>,
-        div
-      )
-    })
-    ReactDOM.unmountComponentAtNode(div)
-  })
-
+describe("FiltersComponent ==> Test UI  ", () => {
   it("SelectedFilters : should render for no selected filter", () => {
     const data = {
       selectedValues: [],
--- src/components/SearchIndexView.js
+++ src/components/SearchIndexView.js
@@ -2,9 +2,8 @@ import React from "react"
 import "../styles/components/searchIndexView.css"
 import ResultComponent from "./resultComponent/ResultComponent"
 import HeaderComponent from "./headerComponent/HeaderComponent"
-import MultiDropDownComponent from "./multiDropDownComponent/MultiDropDownComponent"
-import MultiListComponent from "./multiListComponent/MultiListComponent"
 import {withTranslation} from "react-i18next"
+import FiltersComponent from "./filtersComponent/FiltersComponent"
 import SelectedFiltersComponent from "./filtersComponent/SelectedFiltersComponent"
 
 const SearchIndexView = (props) => {
@@ -12,24 +11,19 @@ const SearchIndexView = (props) => {
     <>
       <HeaderComponent isMobile={props.isMobile}>
         {/* TODO consider using Hidden: https://material-ui.com/components/hidden/ */}
-        {props.multilist.map((list, index) => (
-          <MultiDropDownComponent isMobile={props.isMobile} key={index} {...list} />
-        ))}
+        <FiltersComponent
+          type="dropdown"
+          isMobile={props.isMobile}
+          multilist={props.multilist}
+        />
       </HeaderComponent>
       <div className="sub-container ml-3 mr-3">
-        {
-          <div
-            className={
-              "left-bar left-bar-" +
-              (props.isMobile ? "hide" : "show") +
-              " ml-2 mr-3"
-            }
-          >
-            {props.multilist.map((list, index) => (
-              <MultiListComponent isMobile={props.isMobile} key={index} {...list} />
-            ))}
-          </div>
-        }
+        <FiltersComponent
+          multilist={props.multilist}
+          className={
+            "left-bar left-bar-" + (props.isMobile ? "hide" : "show") + " ml-2 mr-3"
+          }
+        />
         <div className="result-container">
           <SelectedFiltersComponent />
           <ResultComponent />
