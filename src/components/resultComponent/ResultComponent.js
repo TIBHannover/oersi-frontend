a5b317164e46805045487c6e46db831281851a61
Mirjan Hoffmann <mirjan.hoffmann@web.de>
2021-02-10 08:54:59 +0000

    Using more cards in extra large layout
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/46

--- src/components/resultComponent/ResultComponent.js
+++ src/components/resultComponent/ResultComponent.js
@@ -116,7 +116,7 @@ const ResultComponent = (props) => {
           <Grid container direction="row" alignItems="flex-start">
             {setLoading(true)}
             {data.map((item) => (
-              <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
+              <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
                 <TileCard key={item._id} {...item} />
               </Grid>
             ))}
