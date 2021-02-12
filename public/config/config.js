6bd2ec0b8ac937434bc2b9caed5612524e69dfe2
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2021-02-12 10:38:33 +0100

    Adjusted number of cards per page (to better fit to gridlayout)
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/46

--- public/config/config.js
+++ public/config/config.js
@@ -5,8 +5,8 @@ window['runTimeConfig'] = {
     APP_NAME: "oer_data"
   },
   GENERAL_CONFIGURATION:{
-    RESULT_PAGE_SIZE_OPTIONS:["5", "10", "15", "20", "50", "100"],  // page size options configuration    
-    NR_OF_RESULT_PER_PAGE:10,  //  number of results to show per view. Defaults to 10.
+    RESULT_PAGE_SIZE_OPTIONS:["12", "24", "48", "96"],  // page size options configuration    
+    NR_OF_RESULT_PER_PAGE:12,  //  number of results to show per view. Defaults to 12.
     /**
      * Accept a list of objects 
      * example:
