bc175546913cb9d3f35f145d57571c7bf087162d
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2021-05-06 12:10:07 +0200

    Custom filter-item rendering that uses react-window
    This way we render only the items that are currently needed
    
    Ref https://gitlab.com/oersi/oersi-frontend/-/issues/57

--- src/components/multiListComponent/MultiListComponent.js
+++ src/components/multiListComponent/MultiListComponent.js
@@ -3,11 +3,16 @@ import {MultiList} from "@appbaseio/reactivesearch"
 import "./MultiListComponent.css"
 import {getLabelForStandardComponent} from "../../helpers/helpers"
 import {withTranslation} from "react-i18next"
-import Accordion from "@material-ui/core/Accordion"
-import AccordionSummary from "@material-ui/core/AccordionSummary"
-import AccordionDetails from "@material-ui/core/AccordionDetails"
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+} from "@material-ui/core"
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
-import Typography from "@material-ui/core/Typography"
+import {FixedSizeList} from "react-window"
 
 const MultiListComponent = (props) => {
   return (
@@ -20,7 +25,7 @@ const MultiListComponent = (props) => {
         </Typography>
       </AccordionSummary>
       <AccordionDetails>
-        <div className="multilist">
+        <div className="multilist full-width">
           <MultiList
             className={props.className}
             dataField={props.dataField}
@@ -34,17 +39,48 @@ const MultiListComponent = (props) => {
             filterLabel={props.filterLabel.toUpperCase()}
             URLParams={props.URLParams}
             react={{and: props.and}}
-            renderItem={(label, count) =>
-              onItemRender(label, count, props.component, props.t)
-            }
             innerClass={{
-              label: "multilist-label",
               input: "search-component-input",
-              checkbox: "multilist-checkbox",
             }}
             customQuery={props.customQuery}
             defaultQuery={props.defaultQuery}
-          />
+          >
+            {({loading, error, data, value, handleChange}) => {
+              const itemCount = data ? data.length : 0
+              const itemSize = 30
+              const listHeight = Math.min(240, itemCount * itemSize)
+              return (
+                <FixedSizeList
+                  height={listHeight}
+                  itemCount={itemCount}
+                  itemSize={itemSize}
+                  width={"100%"}
+                >
+                  {({index, style}) => (
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={data[index].key in value}
+                          onChange={handleChange}
+                          value={data[index].key}
+                          style={{height: itemSize + "px"}}
+                        />
+                      }
+                      label={onItemRender(
+                        data[index].key,
+                        data[index].doc_count,
+                        props.component,
+                        props.t
+                      )}
+                      className={"mr-0 mb-0 full-width"}
+                      style={delete style.width && style}
+                      classes={{label: "filter-item-label full-width"}}
+                    />
+                  )}
+                </FixedSizeList>
+              )
+            }}
+          </MultiList>
         </div>
       </AccordionDetails>
     </Accordion>
@@ -52,10 +88,10 @@ const MultiListComponent = (props) => {
 }
 export function onItemRender(label, count, component, t) {
   return (
-    <Typography variant="body1" component="span" className="multilist-item">
+    <>
       <div>{getLabelForStandardComponent(label, component, t)}</div>
       <div className="badge badge-info ml-auto">{count}</div>
-    </Typography>
+    </>
   )
 }
 
