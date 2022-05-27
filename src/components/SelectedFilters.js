74f6d78364089803b94a06496ea9b5fa10646771
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2022-05-27 15:38:46 +0200

    added result-stats and selected filters

---
+++ src/components/SelectedFilters.js
@@ -0,0 +1,81 @@
+import React from "react"
+import {SelectedFilters as ReactiveSearchSelectedFilters} from "@appbaseio/reactivesearch"
+import {useTranslation} from "next-i18next"
+import Button from "@mui/material/Button"
+import CloseIcon from "@mui/icons-material/Close"
+import {getLabelForStandardComponent} from "../helpers/helpers"
+import {Box, useTheme} from "@mui/material"
+
+const SelectedFilters = (props) => {
+  const theme = useTheme()
+  const {t} = useTranslation(["translation", "lrt", "subject"])
+  return (
+    <ReactiveSearchSelectedFilters
+      showClearAll={true}
+      clearAllLabel={t("FILTER.CLEAR_ALL")}
+      render={(data) => renderSelectedFilters(data, t, theme)}
+    />
+  )
+}
+
+function renderValue(component, value, isArray, t) {
+  if (isArray && value.length) {
+    const arrayToRender = value.map((item) =>
+      renderValue(component, item, Array.isArray(item), t)
+    )
+    return arrayToRender.join(", ")
+  }
+  return getLabelForStandardComponent(value, component, t)
+}
+
+export function renderSelectedFilters(data, t, theme) {
+  const selectedValues = data.selectedValues
+  const appliedFilters = Object.keys(data.selectedValues)
+  let hasValues = false
+  return (
+    <Box
+      className="selectedFilters"
+      sx={{ml: theme.spacing(1.5), mr: theme.spacing(1.5)}}
+    >
+      {appliedFilters
+        .filter(
+          (id) => data.components.includes(id) && selectedValues[id].showFilter
+        )
+        .map((component) => {
+          const {label, value} = selectedValues[component]
+          const isArray = Array.isArray(value)
+          if (label && ((isArray && value.length) || (!isArray && value))) {
+            hasValues = true
+            return (
+              <Button
+                variant="contained"
+                color="grey"
+                disableElevation
+                sx={{margin: theme.spacing(0.5)}}
+                key={component}
+                onClick={() => data.setValue(component, null)}
+                endIcon={<CloseIcon />}
+              >
+                {t("LABEL." + selectedValues[component].label.toUpperCase())}:{" "}
+                {renderValue(component, value, isArray, t)}
+              </Button>
+            )
+          }
+          return null
+        })}
+      {hasValues ? (
+        <Button
+          variant="contained"
+          color="grey"
+          disableElevation
+          sx={{margin: theme.spacing(0.5)}}
+          onClick={data.clearValues}
+        >
+          {data.clearAllLabel}
+        </Button>
+      ) : null}
+    </Box>
+  )
+}
+
+export default SelectedFilters
--- src/views/Search.js
+++ src/views/Search.js
@@ -3,8 +3,12 @@ import {ReactiveList} from "@appbaseio/reactivesearch"
 import Filters from "../components/Filters"
 import ReactiveSearchComponents from "../config/ReactiveSearchComponents"
 import OersiConfigContext from "../helpers/OersiConfigContext"
+import SelectedFilters from "../components/SelectedFilters"
+import {Box, useTheme} from "@mui/material"
+import ResultStats from "../components/ResultStats"
 
 const Search = (props) => {
+  const theme = useTheme()
   const oersiConfig = React.useContext(OersiConfigContext)
 
   return (
@@ -14,7 +18,17 @@ const Search = (props) => {
         onClose={() => oersiConfig.setFilterViewOpen(false)}
         open={oersiConfig.isFilterViewOpen}
       />
-      <ReactiveList {...ReactiveSearchComponents.resultcard} />
+      <Box
+        aria-label="results"
+        sx={{ml: theme.spacing(1.5), mr: theme.spacing(1.5)}}
+      >
+        <ResultStats sx={{marginX: theme.spacing(1.5)}} />
+        <SelectedFilters />
+        <ReactiveList
+          {...ReactiveSearchComponents.resultcard}
+          showResultStats={false}
+        />
+      </Box>
     </>
   )
 }
