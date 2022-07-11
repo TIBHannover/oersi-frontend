82b7b9af18f29d9c643c86ca2a33d75eabe7f85f
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2022-07-11 10:34:58 +0200

    Revert "fixed availability of scroll-top-button"
    This reverts commit b70dd9be1b60cec0418a5ef0d2156f76c85f71be.

--- src/components/ScrollTop.js
+++ src/components/ScrollTop.js
@@ -24,12 +24,7 @@ const ScrollTop = () => {
           onClick={handleClick}
           size="small"
           aria-label="scroll back to top"
-          sx={{
-            bottom: theme.spacing(2),
-            right: theme.spacing(2),
-            position: "fixed",
-            zIndex: 1800,
-          }}
+          sx={{bottom: theme.spacing(2), right: theme.spacing(2), position: "fixed"}}
         >
           <KeyboardArrowUpIcon />
         </Fab>
