ee26ab304c978dc1417f5dd6e27526bb2ec2844e
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2025-06-19 13:55:01 +0200

    fixed stylesheet loading
    Ref https://gitlab.com/oersi/sidre/sidre-frontend/-/issues/61

--- src/Configuration.js
+++ src/Configuration.js
@@ -8,6 +8,7 @@ import {
 import {SearchIndexFrontendConfigContext} from "./helpers/use-context"
 import {InstantSearch} from "react-instantsearch"
 
+import {Helmet} from "react-helmet"
 import {concatPaths} from "./helpers/helpers"
 
 const useMediaQuery = (query) => {
@@ -151,13 +152,15 @@ const RouterBasedConfig = (props) => {
 
   return (
     <>
-      <link
-        rel="stylesheet"
-        href={concatPaths(
-          GENERAL_CONFIGURATION.PUBLIC_BASE_PATH,
-          "/css/style-override.css"
-        )}
-      />
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href={concatPaths(
+            GENERAL_CONFIGURATION.PUBLIC_BASE_PATH,
+            "/css/style-override.css"
+          )}
+        />
+      </Helmet>
       <SearchIndexFrontendConfigContext.Provider value={frontendConfig}>
         <InstantSearch
           indexName={ELASTIC_SEARCH_INDEX_NAME}
