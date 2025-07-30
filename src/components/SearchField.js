138b66d42e8c63be20248675983d5e6d8647fce5
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2025-07-30 14:40:37 +0200

    implemented debounce function for search field
    Ref https://gitlab.com/oersi/sidre/sidre-frontend/-/issues/111

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect} from "react"
 import {useSearchBox} from "react-instantsearch"
 import Button from "react-bootstrap/Button"
 import {SearchIndexFrontendConfigContext} from "../helpers/use-context"
@@ -7,10 +7,7 @@ import {useTranslation} from "react-i18next"
 import FilterIcon from "./icons/FilterIcon"
 import {useLocation} from "react-router"
 import Form from "react-bootstrap/Form"
-/**
- * SearchField Component
- * creates a search box UI component that is connected to one or more database fields
- */
+
 const SearchField = (props) => {
   const {t} = useTranslation()
   const location = useLocation()
@@ -18,11 +15,27 @@ const SearchField = (props) => {
   const isSearchView = location?.pathname === frontendConfig.routes.SEARCH
   const conf = frontendConfig.searchConfiguration.searchField
   const {query, refine} = useSearchBox()
+  const [searchTerm, setSearchTerm] = React.useState(query)
 
   function onChangeSearchTerm(newSearchTerm) {
-    refine(newSearchTerm)
+    setSearchTerm(newSearchTerm)
   }
-  // TODO implement a debounce function to avoid too many requests
+
+  useEffect(() => {
+    const timer = setTimeout(
+      () => {
+        if (searchTerm !== query) {
+          refine(searchTerm)
+        }
+      },
+      conf.debounce !== undefined ? conf.debounce : 300
+    )
+    return () => clearTimeout(timer)
+  }, [conf.debounce, query, refine, searchTerm])
+
+  useEffect(() => {
+    setSearchTerm(query)
+  }, [query])
 
   return (
     <div
@@ -40,7 +53,7 @@ const SearchField = (props) => {
           }
           type="search"
           placeholder={t("SEARCH_COMPONENT.PLACEHOLDER")}
-          value={query}
+          value={searchTerm}
           onChange={(e) => onChangeSearchTerm(e.currentTarget.value)}
         />
         <button className="search-icon-wrapper">
@@ -66,77 +79,8 @@ const SearchField = (props) => {
           <FilterIcon className="fs-4" />
         </Button>
       )}
-
-      {/*<SearchBox />*/}
-      {/*<DataSearch*/}
-      {/*  componentId={conf.componentId}*/}
-      {/*  className="w-100"*/}
-      {/*  placeholder={t("SEARCH_COMPONENT.PLACEHOLDER")}*/}
-      {/*  dataField={conf.dataField}*/}
-      {/*  fieldWeights={conf.fieldWeights}*/}
-      {/*  queryFormat={conf.queryFormat ? conf.queryFormat : "and"}*/}
-      {/*  fuzziness={conf.fuzziness !== undefined ? conf.fuzziness : 0}*/}
-      {/*  debounce={conf.debounce !== undefined ? conf.debounce : 300}*/}
-      {/*  autosuggest={conf.autosuggest !== undefined ? conf.autosuggest : false}*/}
-      {/*  highlight={conf.highlight}*/}
-      {/*  highlightField={conf.highlightField}*/}
-      {/*  customHighlight={() => ({*/}
-      {/*    highlight: {*/}
-      {/*      pre_tags: ["<mark>"],*/}
-      {/*      post_tags: ["</mark>"],*/}
-      {/*      fields: {*/}
-      {/*        text: {},*/}
-      {/*        title: {},*/}
-      {/*      },*/}
-      {/*      number_of_fragments: 0,*/}
-      {/*    },*/}
-      {/*  })}*/}
-      {/*  innerClass={{*/}
-      {/*    title: "search-title",*/}
-      {/*    input:*/}
-      {/*      "search-component-input search-component-main" +*/}
-      {/*      (frontendConfig.isDarkMode ? " bg-dark" : "") +*/}
-      {/*      (isSearchView ? " input-with-left-filter-icon" : ""),*/}
-      {/*    mic: "search-component-img",*/}
-      {/*  }}*/}
-      {/*  searchInputId="NameSearch"*/}
-      {/*  iconPosition={conf.iconPosition}*/}
-      {/*  showFilter={conf.showFilter}*/}
-      {/*  URLParams={true}*/}
-      {/*  react={{*/}
-      {/*    and: conf.and,*/}
-      {/*  }}*/}
-      {/*  renderNoSuggestion={() => onNoSuggestion()}*/}
-      {/*  renderError={(error) => onError(error)}*/}
-      {/*  renderSelectedTags={() => null}*/}
-      {/*/>*/}
-      {/*{isSearchView && (*/}
-      {/*  <Button*/}
-      {/*    variant="secondary-dark"*/}
-      {/*    className="position-absolute start-0 rounded-circle opacity-75 border-0 bg-transparent lh-1"*/}
-      {/*    aria-label="open filter drawer"*/}
-      {/*    onClick={frontendConfig.onToggleFilterViewOpen}*/}
-      {/*  >*/}
-      {/*    <FilterIcon className="fs-4" />*/}
-      {/*  </Button>*/}
-      {/*)}*/}
     </div>
   )
-  function onError(error) {
-    return (
-      <div>
-        Something went wrong!
-        <br />
-        Error details
-        <br />
-        {error}
-      </div>
-    )
-  }
-
-  function onNoSuggestion() {
-    return <div>No suggestions found</div>
-  }
 }
 
 SearchField.propTypes = {
